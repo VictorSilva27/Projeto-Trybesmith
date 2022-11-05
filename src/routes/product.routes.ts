5fc234befd3cff3d1b7b86670fc033493fd91780
VictorSilva27 <victoradaosilva2@gmail.com>
2022-11-04 23:19:13 -0300

    Requisitos 06 e 07

--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -1,11 +1,18 @@
 import { Router } from 'express';
 import ProductController from '../controllers/product.controller';
+import validateName from '../middlewares/validateName';
+import validateAmount from '../middlewares/validateAmount';
 
 const router = Router();
 
 const productController = new ProductController();
 
 router.get('/', productController.getAll.bind(productController));
-router.post('/', productController.postProduct.bind(productController));
+router.post(
+  '/', 
+  validateName, 
+  validateAmount,
+  productController.postProduct.bind(productController),
+);
 
 export default router;
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,10 +1,21 @@
 import { Router } from 'express';
 import UserController from '../controllers/user.controller';
+import validateUsername from '../middlewares/validateUsername';
+import validateClasse from '../middlewares/validateClasse';
+import validateLevel from '../middlewares/validateLevel';
+import validatePassword from '../middlewares/validatePassword';
 
 const router = Router();
 
 const userController = new UserController();
 
-router.post('/', userController.createUser.bind(userController));
+router.post(
+  '/', 
+  validateUsername,
+  validateClasse,
+  validateLevel,
+  validatePassword, 
+  userController.createUser.bind(userController),
+);
 
 export default router;
